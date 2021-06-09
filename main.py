40d8d9c7bc8ece96e9a2c2b9aea0e3d8e97a83b5
PieDude2612 <arjun.viswanathan2612@gmail.com>
2021-06-09 09:31:49 -0400

    Got plots for train and val set but they seem peculiar...

--- main.py
+++ main.py
@@ -8,40 +8,53 @@ sarsmerscov_val = h5py.File('D:\\ORNL_Code_Data\\sars-mers-cov2_val.h5', 'r')
 label_training = list(open('label_train.txt', 'r'))
 label_validation = list(open('label_val.txt', 'r')) # open all files
 
-print(sarsmerscov_train.keys()) # print all the keys in dataset
-print(sarsmerscov_val.keys())
+print("Successfully loaded all data sets!")
 
-dset_train = np.array(sarsmerscov_train['contact_maps'])
-dset_val = np.array(sarsmerscov_val['contact_maps'])
-print(dset_train.shape)
-print(dset_val.shape) # print the shape of each dataset in samples, feature, feature, 1
+dset_train = np.array(sarsmerscov_train['contact_maps']) # 616207 x 24 x 24 x 1
+dset_val = np.array(sarsmerscov_val['contact_maps']) # 152052 x 24 x 24 x 1
+dset_train.resize((dset_train.shape[0], int((dset_train.shape[1] * dset_train.shape[2]) / 2))) # 616207 x 288
+dset_val.resize((dset_val.shape[0], int((dset_val.shape[1] * dset_val.shape[2]) / 2))) # 152052 x 288
 
-dset_train.resize((dset_train.shape[0], int((dset_train.shape[1] * dset_train.shape[2]) / 2)))
-dset_val.resize((dset_val.shape[0], int((dset_val.shape[1] * dset_val.shape[2]) / 2)))
+print("Resized h5 Files")
 
-print(dset_train.shape)
-print(dset_val.shape)
-
-sars = []
-mers = []
-cov = []
-for i in range(len(dset_train)):
+sars_train = np.zeros((dset_train.shape[0])).astype(int)
+mers_train = np.zeros((dset_train.shape[0])).astype(int)
+cov_train = np.zeros((dset_train.shape[0])).astype(int)
+for i in range(dset_train.shape[0]):
     num = int(str(label_training[i]).strip('\n'))
-    if num == 0: cov.append(dset_train[i, :])
-    elif num == 1: mers.append(dset_train[i, :])
-    elif num == 2: sars.append(dset_train[i, :])
+    if num == 0: cov_train[i] = int(sum(dset_train[i, :]))
+    elif num == 1: mers_train[i] = int(sum(dset_train[i, :]))
+    elif num == 2: sars_train[i] = int(sum(dset_train[i, :])) # find total number of contacts in each sample
 
-print(len(cov))
-print(len(mers))
-print(len(sars)) # print out the lengths
+print("Finished loading training sets")
 
-rowsCov, colsCov = np.where(np.array(cov).astype(int) == 1)
-rowsMers, colsMers = np.where(np.array(mers).astype(int) == 1)
-rowsSars, colsSars = np.where(np.array(sars).astype(int) == 1)
+sars_val = np.zeros((dset_val.shape[0])).astype(int)
+mers_val = np.zeros((dset_val.shape[0])).astype(int)
+cov_val = np.zeros((dset_val.shape[0])).astype(int)
+for i in range(dset_val.shape[0]):
+    num = int(str(label_validation[i]).strip('\n'))
+    if num == 0: cov_val[i] = int(sum(dset_val[i, :]))
+    elif num == 1: mers_val[i] = int(sum(dset_val[i, :]))
+    elif num == 2: sars_val[i] = int(sum(dset_val[i, :])) # find total number of contacts in each sample
 
-plt.scatter(rowsCov, colsCov, 'b')
-plt.scatter(rowsMers, colsMers, 'r')
-plt.scatter(rowsSars, colsSars, 'k')
-plt.xlabel('Contacts')
-plt.ylabel('Samples')
-plt.show()
+print("Finished loading validation sets")
+print("Plotting...")
+
+train_fig = plt.figure(1)
+train_fig.add_subplot(111)
+plt.scatter(np.arange(0, dset_train.shape[0]), sars_train, c='b')
+plt.scatter(np.arange(0, dset_train.shape[0]), mers_train, c='r')
+plt.scatter(np.arange(0, dset_train.shape[0]), cov_train, c='k')
+plt.ylabel('Total Contacts')
+plt.xlabel('Samples (Training Set)')
+
+val_fig = plt.figure(2)
+val_fig.add_subplot(111)
+plt.scatter(np.arange(0, dset_val.shape[0]), sars_val, c='b')
+plt.scatter(np.arange(0, dset_val.shape[0]), mers_val, c='r')
+plt.scatter(np.arange(0, dset_val.shape[0]), cov_val, c='k')
+plt.ylabel('Total Contacts')
+plt.xlabel('Samples (Validation Set)')
+
+print("Finished plotting")
+plt.show() # plot both sets
