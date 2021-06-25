5035218a3136ec56c697d15c761f77e411038e44
PieDude2612 <arjun.viswanathan2612@gmail.com>
2021-06-25 10:21:11 -0400

    Made changes to load data files

--- Code/hea_dataset/kmeans/kmeans_load_data.py
+++ Code/hea_dataset/kmeans/kmeans_load_data.py
@@ -1,28 +1,33 @@
+##
 import numpy as np
 import time
 from sklearn.utils import shuffle
 
+##
 print(str(time.ctime()) + ": Initializing...")
-trainset = np.load('/gpfs/alpine/gen150/proj-shared/junqi/hea/HEA_train.npy')
-valset = np.load('/gpfs/alpine/gen150/proj-shared/junqi/hea/HEA_val.npy')
-label_training = np.load('/gpfs/alpine/gen150/proj-shared/junqi/hea/label_T_train.npy')
-label_validation = np.load('/gpfs/alpine/gen150/proj-shared/junqi/hea/label_T_val.npy')
+ts = np.load('/gpfs/alpine/gen150/proj-shared/junqi/hea/HEA_train.npy')
+vs = np.load('/gpfs/alpine/gen150/proj-shared/junqi/hea/HEA_val.npy')
+lt = np.load('/gpfs/alpine/gen150/proj-shared/junqi/hea/label_T_train.npy')
+lv = np.load('/gpfs/alpine/gen150/proj-shared/junqi/hea/label_T_val.npy')
+print(str(time.ctime()) + ": Successfully loaded all data sets!")
 
-print(trainset.shape)
-print(valset.shape)
-print(label_training.shape)
-print(label_validation.shape)
+##
+print(ts.shape)
+print(vs.shape)
+print(lt.shape)
+print(lv.shape)
 
-trainset, label_training = shuffle(trainset, label_training, random_state=0)
-valset, label_validation = shuffle(valset, label_validation, random_state=0)
-        
-# train_size = 40000
-# val_size = 10000
-# trainset = trainset[0:train_size]
-# valset = valset[0:val_size]
-# label_training = label_training[0:train_size]
-# label_validation = label_validation[0:val_size]
+##
+ts, lt = shuffle(ts, lt, random_state=0)
+vs, lv = shuffle(vs, lv, random_state=0)
 
+##
+train_size = 4000
+val_size = 1000
+trainset = ts[0:train_size]
+valset = vs[0:val_size]
+label_training = lt[0:train_size]
+label_validation = lv[0:val_size]
 
 np.savez('savefile.npz', train=trainset, val=valset, labval=label_validation)
 
