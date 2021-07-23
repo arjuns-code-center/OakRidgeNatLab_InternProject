d27c0018021a48a80bc172050dc0feb924e2eb0e
PieDude2612 <arjun.viswanathan2612@gmail.com>
2021-07-23 12:28:07 -0400

    Updated all files as of progress 7/23/21

--- Code/kmeans/kmeans_init.py
+++ Code/kmeans/kmeans_init.py
@@ -4,15 +4,14 @@ import pandas as pd
 import time, argparse
 from sklearn.cluster import KMeans as sk_kmeans
 from cuml.cluster import KMeans as cuml_kmeans
-from dask_ml.cluster import KMeans as cuml_dask_kmeans
+from cuml.dask.cluster import KMeans as dask_kmeans
 from dask_cuda import LocalCUDACluster
 from dask.distributed import Client
-import cudf, dask_cudf
-import dask
-import pandas as pd
+import cudf, dask_cudf, dask
 
 args = argparse.ArgumentParser()
 args.add_argument('--npartitions', type=int, help='number of data partitions')
+args.add_argument('--single_gpu', type=bool, help='single or multi gpu')
 args.add_argument('--dataset', type=str, help='type of data loading in')
 args = args.parse_args()
 npartitions = args.npartitions
@@ -22,46 +21,57 @@ datatype = args.dataset
 def sklearn_km(train_pca, val_pca, nclusters):
     print(str(time.ctime()) + ": Implementing KMeans Clustering with Sklearn...")
     start = time.time()
+    
     kmeans = sk_kmeans(n_clusters=nclusters, random_state=0)
     kmeans.fit(train_pca)
     labels_val = np.array(kmeans.predict(val_pca))
+    
     end = time.time()
     sk_diff = round(end - start, 2)
     print(str(time.ctime()) + ": Finished KMeans Clustering with Sklearn in: " + str(sk_diff) + " seconds!")
+    
     return labels_val
-        
-def rapids_km(train_pca, val_pca, nclusters):
-    print(str(time.ctime()) + ": Transferring CPU->GPU...")
-    cluster = LocalCUDACluster(n_workers=npartitions, threads_per_worker=4)
-    client  = Client(cluster) # use client scheduler for multi GPU
-    
-    d_tpca = dask_cudf.from_cudf(cudf.DataFrame.from_pandas(pd.DataFrame(train_pca)), npartitions=npartitions)
-    d_vpca = dask_cudf.from_cudf(cudf.DataFrame.from_pandas(pd.DataFrame(val_pca)), npartitions=npartitions)
-        
-#     d_tpca = d_tpca.persist()
-#     d_vpca = d_vpca.persist()
 
-    print(str(time.ctime()) + ": Sucessfully Transferred!")
-    print(str(time.ctime()) + ": Implementing KMeans Clustering with Rapids...")
+def rapids_km(train_pca, val_pca, nclusters, single_gpu):
+    kmeans = None
+    name = None
     
-    kmeans = cuml_dask_kmeans(n_clusters=nclusters, random_state=0)
+    if single_gpu:
+        print(str(time.ctime()) + ": Transferring CPU->GPU...")
+        train_pca = cp.array(train_pca)
+        val_pca = cp.array(val_pca)
+        print(str(time.ctime()) + ": Successfully Transferred!")
+
+        name = "Rapids"
+        kmeans = cuml_kmeans(n_clusters=nclusters, random_state=0)
+    else:
+        print(str(time.ctime()) + ": Transferring CPU->GPUs...")
+        train_pca = dask_cudf.from_dask_dataframe(dask.dataframe.from_pandas(pd.DataFrame(train_pca)), npartitions=npartitions).persist()
+        val_pca = dask_cudf.from_dask_dataframe(dask.dataframe.from_pandas(pd.DataFrame(val_pca)), npartitions=npartitions).persist()
+        print(str(time.ctime()) + ": Successfully Transferred!")
+
+        cluster = LocalCUDACluster(n_workers=npartitions, threads_per_worker=1)
+        client  = Client(cluster)
+
+        name = "Dask"
+        kmeans = dask_kmeans(n_clusters=nclusters, random_state=0)
+        
+    print(str(time.ctime()) + ": Implementing KMeans Clustering with " + name + "...")
+    kmeans.fit(train_pca)
     runtimes = np.array([])
     
     for i in range(10):
         start = time.time()
-        kmeans.fit(d_tpca)
-        labels_val = cp.asnumpy(cp.array(kmeans.predict(d_vpca)))
-        labels = labels.result()
+        labels_val = cp.asnumpy(cp.array(kmeans.predict(val_pca)))
         end = time.time()
         r_diff = end - start
         
         runtimes = np.append(runtimes, r_diff)
     
     avg_runtime = round(sum(runtimes[3:]) / len(runtimes[3:]), 2)
-    
-    print(str(time.ctime()) + ": Finished KMeans Clustering with Rapids in: " + str(avg_runtime) + " seconds!")
+    print(str(time.ctime()) + ": Finished KMeans Clustering with " + name + " in: " + str(avg_runtime) + " seconds!")
     return labels_val
-
+    
 train_pca = None
 val_pca = None
 npzfile = None
@@ -86,21 +96,21 @@ l = np.array([]).astype(int)
 for i in range(len(lv_onehot)):
     l = np.append(l, np.argmax(lv_onehot[i]))
 
-# sk_labels_val = sklearn_km(train_pca, val_pca, nc)
-# accuracy = (sum(sk_labels_val == l) / len(l)) * 100
-# print('Accuracy: {}'.format(accuracy))
+sk_labels_val = sklearn_km(train_pca, val_pca, nc)
+accuracy = (sum(sk_labels_val == l) / len(l)) * 100
+print('Accuracy: {}'.format(accuracy))
 
-r_labels_val = rapids_km(train_pca, val_pca, nc) 
+r_labels_val = rapids_km(train_pca, val_pca, nc, single_gpu)
 accuracy = (sum(r_labels_val == l) / len(l)) * 100
 print('Accuracy: {}'.format(accuracy))
 
 if datatype == 'SARSMERSCOV2':
     npzfile2 = np.load('/gpfs/alpine/gen150/scratch/arjun2612/ORNL_Coding/Code/pca/smc2_sk_clusterfiles.npz')
     reduced_val = npzfile2['redval']
-    # np.savez('smc2_sk_plotting.npz', reslab=sk_labels_val, lv=label_validation, redval=reduced_val)
+    np.savez('smc2_sk_plotting.npz', reslab=sk_labels_val, lv=label_validation, redval=reduced_val)
     np.savez('smc2_r_plotting.npz', reslab=r_labels_val, redval=reduced_val, lv=label_validation)
 elif datatype == 'HEA':
     npzfile2 = np.load('/gpfs/alpine/gen150/scratch/arjun2612/ORNL_Coding/Code/pca/hea_sk_clusterfiles.npz')
     reduced_val = npzfile2['redval']
-    # np.savez('hea_sk_plotting.npz', reslab=sk_labels_val, lv=label_validation, redval=reduced_val)
+    np.savez('hea_sk_plotting.npz', reslab=sk_labels_val, lv=label_validation, redval=reduced_val)
     np.savez('hea_r_plotting.npz', reslab=r_labels_val, redval=reduced_val, lv=label_validation)
--- Code/pca/pca_init.py
+++ Code/pca/pca_init.py
@@ -4,18 +4,21 @@ import pandas as pd
 import time, argparse
 from sklearn.preprocessing import normalize
 from sklearn.decomposition import PCA as sk_PCA
-from cuml.dask.decomposition import PCA as cuml_PCA
-from dask.distributed import Client, LocalCluster
+from cuml.decomposition import PCA as cuml_PCA
+from cuml.dask.decomposition import PCA as dask_PCA
+from dask.distributed import Client
 from dask_cuda import LocalCUDACluster
 import cudf, dask, dask_cudf
 
 args = argparse.ArgumentParser()
-args.add_argument('--schedulerfile', type=str, help='scheduler file for client')
+#args.add_argument('--schedulerfile', type=str, help='scheduler file for client')
 args.add_argument('--npartitions', type=int, help='number of data partitions')
+args.add_argument('--single_gpu', type=bool, help='single gpu')
 args.add_argument('--dataset', type=str, help='type of data loading in')
 args = args.parse_args()
-sfile = args.schedulerfile
+#sfile = args.schedulerfile
 npartitions = args.npartitions
+single_gpu = args.single_gpu
 datatype = args.dataset
 
 def sklearn_pca(normalized_train_pca, normalized_val_pca):
@@ -35,77 +38,85 @@ def sklearn_pca(normalized_train_pca, normalized_val_pca):
     print(str(time.ctime()) + ": Finished PCA Clustering with Sklearn in: " + str(sk_diff) + " seconds!")
     return reduced_train, reduced_val
     
-def rapids_pca(normalized_train_pca, normalized_val_pca, parts, c):
+def rapids_pca(ntpca, nvpca, single_gpu, parts):    
     reduced_train = np.array([])
     reduced_val = np.array([])
-    
-    print(str(time.ctime()) + ": Transferring CPU->GPUs...")        
+    pca = None
+    name = None
+        
+    if single_gpu:
+        print(str(time.ctime()) + ": Transferring CPU->GPU...")
+        ntpca = cp.array(ntpca)
+        nvpca = cp.array(nvpca)
+        print(str(time.ctime()) + ": Successfully transferred!")
+        
+        start = time.time()
+        
+        name = "Rapids"
+        pca = cuml_PCA(n_components=2)  # 2 PCs
+    else:
+        print(str(time.ctime()) + ": Transferring CPU->GPUs...")
+        cluster = LocalCUDACluster(n_workers=npartitions, threads_per_worker=1)
+        client  = Client(cluster)
+        
+        ntpca = dask_cudf.from_cudf(cudf.DataFrame.from_pandas(pd.DataFrame(ntpca)), npartitions=parts).persist()
+        nvpca = dask_cudf.from_cudf(cudf.DataFrame.from_pandas(pd.DataFrame(nvpca)), npartitions=parts).persist()
+        
+        print(str(time.ctime()) + ": Successfully transferred!")
+        
+        name = "Dask"
+        pca = dask_PCA(n_components=2)
 
-    d_ntpca = dask_cudf.from_cudf(cudf.DataFrame(normalized_train_pca), npartitions=parts).persist()
-    d_nvpca = dask_cudf.from_cudf(cudf.DataFrame(normalized_val_pca), npartitions=parts).persist()
-    
-    print(str(time.ctime()) + ": Successfully transferred!")
-    
-    print(str(time.ctime()) + ": Implementing PCA Clustering with Rapids...")
-    pca = cuml_PCA(n_components=2, client=c)  # 2 PCs
-    pca.fit(d_ntpca)
+    print(str(time.ctime()) + ": Implementing PCA Clustering with " + name + "...")
+    pca.fit(ntpca)
     runtimes = np.array([])
-    
+        
     for i in range(10):
         start = time.time()
-        reduced_train = cp.array(pca.transform(d_ntpca))
-        reduced_val = cp.array(pca.transform(d_nvpca)) # reduce dimensions of both sets
+        reduced_train = cp.array(pca.transform(ntpca))
+        reduced_val = cp.array(pca.transform(nvpca)) # reduce dimensions of both sets
         end = time.time()
         r_diff = end - start
-        
-        runtimes = np.append(runtimes, r_diff)
-    
-    avg_runtime = round(sum(runtimes[3:]) / len(runtimes[3:]), 2)
-    
-    print(str(time.ctime()) + ": Finished PCA Clustering with Rapids in: " + str(avg_runtime) + " seconds!")
-    print('Total explained variance: {}'.format(pca.explained_variance_ratio_.sum() * 100))
 
+        runtimes = np.append(runtimes, r_diff)
+
+    avg_runtime = round(sum(runtimes[3:]) / len(runtimes[3:]), 2)
+
+    print(str(time.ctime()) + ": Finished PCA Clustering with " + name + " in: " + str(avg_runtime) + " seconds!")
+    print('Total explained variance: {}'.format(pca.explained_variance_ratio_.sum() * 100))
+    
     return reduced_train, reduced_val
     
-if __name__ == '__main__':
-    print("Initializing Client...")
-    client  = Client(scheduler_file=sfile)
-    client.wait_for_workers(n_workers=npartitions)
-    print("Successfully Initialized Cluster and Client!")
-    
-    print(str(time.ctime()) + ": Initializing...")
-    train_pca = None
-    val_pca = None
+print(str(time.ctime()) + ": Initializing...")
+train_pca = None
+val_pca = None
 
-    if datatype == 'SARSMERSCOV2':
-        training = np.array([])
-        validation = np.array([])
+if datatype == 'SARSMERSCOV2':
+    training = np.array([])
+    validation = np.array([])
 
-        npzfile = np.load('/gpfs/alpine/gen150/scratch/arjun2612/ORNL_Coding/Code/sars_mers_cov2_dataset/smc2_dataset.npz')
-        training = npzfile['train3D']
-        validation = npzfile['val3D']
+    npzfile = np.load('/gpfs/alpine/gen150/scratch/arjun2612/ORNL_Coding/Code/sars_mers_cov2_dataset/smc2_dataset.npz')
+    training = npzfile['train3D']
+    validation = npzfile['val3D']
 
-        train_pca = np.reshape(training, (training.shape[0], -1))  # 60000 x 576
-        val_pca = np.reshape(validation, (validation.shape[0], -1))  # 15000 x 576
-    elif datatype == 'HEA':
-        npzfile = np.load('/gpfs/alpine/gen150/scratch/arjun2612/ORNL_Coding/Code/hea_dataset/hea_dataset.npz')
-        train_pca = npzfile['train']
-        val_pca = npzfile['val']
+    train_pca = np.reshape(training, (training.shape[0], -1))  # 60000 x 576
+    val_pca = np.reshape(validation, (validation.shape[0], -1))  # 15000 x 576
+elif datatype == 'HEA':
+    npzfile = np.load('/gpfs/alpine/gen150/scratch/arjun2612/ORNL_Coding/Code/hea_dataset/hea_dataset.npz')
+    train_pca = npzfile['train']
+    val_pca = npzfile['val']
 
-    print(str(time.ctime()) + ": Successfully loaded all data sets!")
+print(str(time.ctime()) + ": Successfully loaded all data sets!")
 
-    normalized_train_pca = normalize(train_pca, axis=1, norm='l1')
-    normalized_val_pca = normalize(val_pca, axis=1, norm='l1')
+normalized_train_pca = normalize(train_pca, axis=1, norm='l1')
+normalized_val_pca = normalize(val_pca, axis=1, norm='l1')
 
-    # sk_rt, sk_rv = sklearn_pca(normalized_train_pca, normalized_val_pca)
-    r_rt, r_rv = rapids_pca(cp.array(normalized_train_pca), cp.array(normalized_val_pca), npartitions, client)
+sk_rt, sk_rv = sklearn_pca(normalized_train_pca, normalized_val_pca)
+r_rt, r_rv = rapids_pca(normalized_train_pca, normalized_val_pca, single_gpu, npartitions)
 
-    if datatype == 'SARSMERSCOV2':
-        # np.savez('smc2_sk_clusterfiles.npz', redtrain=sk_rt, redval=sk_rv)
-        cp.savez('smc2_r_clusterfiles.npz', redtrain=r_rt, redval=r_rv)
-    elif datatype == 'HEA':
-        # np.savez('hea_sk_clusterfiles.npz', redtrain=sk_rt, redval=sk_rv)
-        cp.savez('hea_r_clusterfiles.npz', redtrain=r_rt, redval=r_rv)
-        
-    client.retire_workers(list(client.scheduler_info()['workers']), close_workers=True)
-    client.shutdown()
+if datatype == 'SARSMERSCOV2':
+    np.savez('smc2_sk_clusterfiles.npz', redtrain=sk_rt, redval=sk_rv)
+    cp.savez('smc2_r_clusterfiles.npz', redtrain=r_rt, redval=r_rv)
+elif datatype == 'HEA':
+    np.savez('hea_sk_clusterfiles.npz', redtrain=sk_rt, redval=sk_rv)
+    cp.savez('hea_r_clusterfiles.npz', redtrain=r_rt, redval=r_rv)
